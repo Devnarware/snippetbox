f3d57e2381fcc4a7eb92ee9380c7acb4767ac47f
Devendra Kumar Narware <devn2405@gmail.com>
2026-04-20 01:54:40 +0530

    made the first server

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+
+	log.Print("starting the server on :4000")
+
+	http.ListenAndServe(":4000", mux)
+}
+
+func home(w http.ResponseWriter, r *http.Request){
+	_, err := w.Write([]byte("hello from snippetbox"))
+
+	if err != nil {
+		http.Error(w, "idk", http.StatusNotAcceptable)
+	}
 }
