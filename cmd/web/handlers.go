fbd7511d0dffbf6b8015b5e4feee864dc61cd07e
Devendra Kumar Narware <devn2405@gmail.com>
2026-04-24 02:21:27 +0530

    feat: enhance handlers with server header and response messages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,5 +4,28 @@ import "net/http"
 
 func home(w http.ResponseWriter, r *http.Request){
 	
+	w.Header().Add("server", "GO")
+	w.Write([]byte("Hello from snippet box"))
+
+}
+// this is our Home function, which will handle the "/" route
+
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	
+	w.Write([]byte("Hello from snippet box"))
+}
+
+
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	
+	w.Write([]byte("Hello from snippet box"))
+}
+
+
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request){
+	
 	w.Write([]byte("Hello from snippet box"))
 }
