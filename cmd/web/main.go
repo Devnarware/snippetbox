7fa9c4a71c0734530478c17d6960ab1bd1a74d69
Devendra Kumar Narware <devn2405@gmail.com>
2026-04-24 02:21:10 +0530

    Add more functions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,8 +9,12 @@ import (
 func main(){
 
 	mux := http.NewServeMux()
-	
-	mux.HandleFunc("GET /", home)
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POSt /snippet/create", snippetCreatePost)
+	// create all the handler function
 
 	fmt.Printf("Starting the server at port :4000")
 
