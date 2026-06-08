d4184b269239ca906d28022991a2064f49999d62
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-08 23:04:14 +0530

    fix: update logging for server start and use flag for address configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main(){
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
 	mux := http.NewServeMux()
 
@@ -15,17 +18,17 @@ func main(){
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	// stripPrefix() -> it is used to remove the prefix from the request URL before passing it to the file server, it is used to serve static files from the "./ui/static" directory when the request URL starts with "/static/"
 
+	// .handle -> 
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	// create all the handler function
 
-	fmt.Printf("Starting the server at port :4000")
+	log.Printf("Server is starting at the port: %s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 
-	if err != nil {
-		log.Fatal(err) ;
-	}
+	log.Fatal(err)
 }
--- zNotes_and_learnings/main.go
+++ zNotes_and_learnings/main.go
@@ -65,7 +65,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 	//strconv -> it is a package in go which is used to convert the string to other data types like int, float, etc
 	// Atoi -> it is used to convert the string to integer
-	// PathValue -> it is used to get the value of the path parameter
+	// PathValue -> it is u	sed to get the value of the path parameter
  
 	if err != nil || id < 1 {
 		http.NotFound(w,r)
