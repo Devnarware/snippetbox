27b076211ac3e738561d349b201ee1c25ac09dd2
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-10 20:19:59 +0530

    fix: remove unnecessary comment in main function for clarity

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -29,8 +29,6 @@ func main(){
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	// stripPrefix() -> it is used to remove the prefix from the request URL before passing it to the file server, it is used to serve static files from the "./ui/static" directory when the request URL starts with "/static/"
 
-	// .handle -> 
-
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
