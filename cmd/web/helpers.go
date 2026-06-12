d1057ed6e510b215acf0cf60b50c3744e3b27d26
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-12 18:26:06 +0530

    fix: add missing newline at end of file in helpers.go

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -13,3 +13,8 @@ func (app *application)serverError(w http.ResponseWriter, r *http.Request, err e
 	app.logger.Error(err.Error(), "method", method, "uri", uri)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
+
+
+func (app *application)clientError(w http.ResponseWriter, r *http.Request, status int){
+	http.Error(w, http.StatusText(status), status)
+}
