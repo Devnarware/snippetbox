592545f59339261cd7ecac1f929b667da9c7419f
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-12 18:38:55 +0530

    refactor: streamline route handling by utilizing app.routes() method

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,23 +28,13 @@ func main(){
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	// stripPrefix() -> it is used to remove the prefix from the request URL before passing it to the file server, it is used to serve static files from the "./ui/static" directory when the request URL starts with "/static/"
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	// create all the handler function
-	// we have to use app.func because we have to use the custom logger in the handler function, and the custom logger is a field of the application struct, so we have to use app.func to access the custom logger in the handler function
+	mux := app.routes()
+	// calling the route function to get the server mux so that we can serve it
 
 	logger.Info("Starting the server", "addr", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
+	// we can directly call the route function here without saving it in a variable
 
 	logger.Error(err.Error())
 	os.Exit(1)
